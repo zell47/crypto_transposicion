84d34cea3bdfe4fd8ffc3740b92462d24a76cb6d
jmorales <jmorales@jmorales-VirtualBox>
2013-09-25 23:17:20 +0200

    no me funciona la union de la frase. falta desencriptacion...

--- transposicion.c
+++ transposicion.c
@@ -11,23 +11,42 @@
 
 void main()
 {
-  char string[MAX],string2[MAX];
-  int i=0;
-  int ancho,pos;
-  printf("Dime una palabra:");
-  gets(string);
-  ancho=strlen(string);
-  while (i!=ancho){
-	  pos=0;
-	  printf("Dime la posicion de la letra %d: ",i+1);
-	  scanf("%d",&pos);
-	  string2[i] = string[pos-1];
-	  i++;
-  }
-  if (i==ancho){
-	  string2[i]='\0';
-  }
-  printf("%s\n",string2);
+	char string[MAX],string2[MAX],string3[MAX],caracter;
+	int i=0,j=0,k=0;
+	int ancho,pos,op;
+	printf("Encriptacion por transposicion\n");
+	printf("Que deseas hacer?\n");
+	printf("1.Encriptar\n");
+	printf("1.Desencriptar\n");
+	scanf("%d",&op);
+	getchar();
+	if (op==1){
+		printf("Letras a encriptar:");
+		gets(string);
+		printf("%s\n",string);
+		for (j=0;caracter !='32' || caracter!='0';j++){
+			caracter = string[j];
+			if (caracter !='32'){
+				string3[k] = caracter;
+				k++;
+			}
+		}
+		string3[i]='\0';
+		printf("%s\n",string3);
+		ancho=strlen(string3);
+		while (i!=ancho){
+			pos=0;
+			printf("Dime la posicion de la letra %d: ",i+1);
+			scanf("%d",&pos);
+			string2[i] = string3[pos-1];
+			i++;
+		}
+		if (i==ancho){
+			string2[i]='\0';
+		}
+		printf("%s\n",string2);
+	}
+
 }
 
 
