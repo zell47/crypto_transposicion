4b329207d8023b391683ddccbe53199bb0163176
jmorales <jmorales@jmorales-VirtualBox>
2013-09-25 12:59:14 +0200

    Funcionando transposicion pidiendo palabra y orden.

--- transposicion.c
+++ transposicion.c
@@ -7,16 +7,27 @@
 
 #include <string.h>
 #include <stdio.h>
+#define MAX 500
 
 void main()
 {
-  char string[9] = "Hola";
-  printf("%s\n",string);
-  string[3] = 'H';
-  string[1] = 'o';
-  string[2] = 'l';
-  string[0] = 'a';
-  printf("%s\n",string);
+  char string[MAX],string2[MAX];
+  int i=0;
+  int ancho,pos;
+  printf("Dime una palabra:");
+  gets(string);
+  ancho=strlen(string);
+  while (i!=ancho){
+	  pos=0;
+	  printf("Dime la posicion de la letra %d: ",i+1);
+	  scanf("%d",&pos);
+	  string2[i] = string[pos-1];
+	  i++;
+  }
+  if (i==ancho){
+	  string2[i]='\0';
+  }
+  printf("%s\n",string2);
 }
 
 
